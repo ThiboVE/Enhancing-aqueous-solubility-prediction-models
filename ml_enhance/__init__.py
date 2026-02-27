79cbf5126f142ed004493acd5c0f88bd6bbccb14
Thibo Van Eeckhoorn <Thibo.Vaneeckhoorn@ugent.be>
2026-02-27 14:10:06 +0100

    added solubility + saved processed dataset

--- ml_enhance/__init__.py
+++ ml_enhance/__init__.py
@@ -1,5 +1,6 @@
 """Library for training ML models using topological and QFP descriptors."""
 
+from ml_enhance.general_functions import parallelize
 from ml_enhance.preprocess_smiles import get_preprocessed_smiles, is_atom, is_salt, set_atom_map_numbers
 from ml_enhance.qfp_processing import (
     ConformerAggregator,
@@ -22,5 +23,6 @@ __all__ = [
     "is_atom",
     "is_salt",
     "norm_intensity",
+    "parallelize",
     "set_atom_map_numbers",
 ]
