49f27109779690525efb60e10f7d49898d87b3f4
Thibo Van Eeckhoorn <Thibo.Vaneeckhoorn@ugent.be>
2026-04-21 10:02:06 +0200

    reran 'relevant' calculations

--- relevant_feature_filter.py
+++ relevant_feature_filter.py
@@ -165,6 +165,7 @@ def main() -> None:
     df["n_atoms"] = df["smiles"].map(get_num_atoms)
     # df = normalize(df)
 
+    # df.to_csv(r"./data/processed_dataset_wo_metals_w_even_more_qm2_relevant.csv", index=False)
     df.to_csv(r"./data/processed_dataset_wo_metals_w_even_more_qm2_relevant_no_norm.csv", index=False)
 
 
