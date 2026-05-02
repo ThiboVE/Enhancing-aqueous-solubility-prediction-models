6dc9ee72e02ebfcf2869507e3ab500801b3d7a1e
Thibo Van Eeckhoorn <Thibo.Vaneeckhoorn@ugent.be>
2026-05-02 15:02:42 +0200

    downloaded + processed rerun dataset

--- relevant_feature_filter.py
+++ relevant_feature_filter.py
@@ -158,15 +158,15 @@ def normalize(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def main() -> None:
-    df = pd.read_csv(r"data\processed_dataset_wo_metals_w_even_more_qm2.csv")
+    df = pd.read_csv(r"data\processed_dataset_rerun.csv")
     df = df.copy()
 
     df = filter_df(df)
     df["n_atoms"] = df["smiles"].map(get_num_atoms)
-    # df = normalize(df)
+    df = normalize(df)
 
-    # df.to_csv(r"./data/processed_dataset_wo_metals_w_even_more_qm2_relevant.csv", index=False)
-    df.to_csv(r"./data/processed_dataset_wo_metals_w_even_more_qm2_relevant_no_norm.csv", index=False)
+    df.to_csv(r"./data/processed_dataset_rerun_relevant.csv", index=False)
+    # df.to_csv(r"./data/processed_dataset_rerun_relevant_no_norm.csv", index=False)
 
 
 if __name__ == "__main__":
