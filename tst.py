62304d1b11a01dcd2eebec1967756e9c4cd7ff08
Thibo Van Eeckhoorn <Thibo.Vaneeckhoorn@ugent.be>
2026-05-28 16:56:26 +0200

    fdsq

--- tst.py
+++ tst.py
@@ -1,15 +1,37 @@
+# import re
+# from collections.abc import Sequence
 # from pathlib import Path
 
 
-# def main() -> None:
-#     path = Path("1_KRR_combo_relevant")
-#     for obj in path.glob("*"):
-#         name = obj.name
-#         parts = name.split("_")
-#         parts[4] = f"id={parts[4]}"
-#         new_name = "_".join(parts)
+# def group_ranges(nums: Sequence[int]) -> list[str]:
+#     if not nums:
+#         return []
 
-#         obj.rename(obj.parent / new_name)
+#     ranges = []
+#     start = prev = nums[0]
+
+#     for n in nums[1:]:
+#         if n == prev + 1:
+#             prev = n
+#         else:
+#             ranges.append(f"{start}-{prev}" if start != prev else str(start))
+#             start = prev = n
+
+#     ranges.append(f"{start}-{prev}" if start != prev else str(start))
+
+#     return ranges
+
+
+# def main() -> None:
+#     path = Path("run_dft_rerun/results")
+
+#     files: list[Path] = list(path.glob("*"))
+
+#     ids = [int(re.search(r"\d+", file.stem).group(0)) for file in files]
+
+#     failed = [id for id in range(1000) if id not in ids]
+
+#     print(",".join(group_ranges(failed)))
 
 
 # if __name__ == "__main__":
